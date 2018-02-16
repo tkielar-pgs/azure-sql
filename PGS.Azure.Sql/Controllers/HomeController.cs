d8122c8afd92a75ad21b29cf32f1af88f1b182f0
Tomasz Kielar <tkielar@pgs-soft.com>
2018-02-16 15:29:19 +0100

    Empty project

---
+++ PGS.Azure.Sql/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PGS.Azure.Sql.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();        
+    }
+}
